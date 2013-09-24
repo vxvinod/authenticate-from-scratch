33ab987d7ab210115ca97446342e3bf52b6957d3
vxvinod <vinodkmrmurugan@gmail.com>
2013-09-25 01:12:53 +0530

    Login and sign up page done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthenticateFromSearch::Application.config.secret_token = 'ab8d9fc9863ed160b247478bb99f39685dfa338d3dc3666e9bd1822e6fb81037d15045e2580746013be5a08acc7c34989a474024c75e1f61fb1e36770b4cf83e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AuthenticateFromSearch::Application.config.session_store :cookie_store, key: '_authenticate-from-search_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AuthenticateFromSearch::Application.config.session_store :active_record_store
